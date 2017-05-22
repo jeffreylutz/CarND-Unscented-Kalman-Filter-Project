2334439a34dda8a038bc05dbf1176872115aa302
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2017-05-22 15:33:38 -0400

    Fixed UKF algorithm

--- src/ukf.h
+++ src/ukf.h
@@ -13,23 +13,9 @@ using Eigen::VectorXd;
 
 class UKF {
 public:
-	
-  //************************************************************************ 
-  ///* measurements
-  float ro_in;
-  float phi_in;
-  float ro_dot_in;
-
-  ///* tool object used to compute RMSE
-  Tools tools;
-	
-  //*************************************************************************
 
   ///* initially set to false, set to true in first call of ProcessMeasurement
   bool is_initialized_;
-
-  ///* previous timestamp
-  long previous_timestamp_;
 
   ///* if this is false, laser measurements will be ignored (except for init)
   bool use_laser_;
@@ -40,15 +26,9 @@ public:
   ///* state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate] in SI units and rad
   VectorXd x_;
 
-  ///* aug state vector: [pos1 pos2 vel_abs yaw_angle yaw_rate nu psi] in SI units and rad
-  VectorXd x_aug_;
-
   ///* state covariance matrix
   MatrixXd P_;
 
-  ///* aug state covariance matrix
-  MatrixXd P_aug_;
-  
   ///* predicted sigma points matrix
   MatrixXd Xsig_pred_;
 
@@ -85,6 +65,9 @@ public:
   ///* Augmented state dimension
   int n_aug_;
 
+  ///* Number of sigma points
+  int n_sig_;
+
   ///* Sigma point spreading parameter
   double lambda_;
 
@@ -93,6 +76,12 @@ public:
 
   ///* the current NIS for laser
   double NIS_laser_;
+
+  ///* Radar measurement noise covariance matrix
+  MatrixXd R_radar_;
+
+  ///* Lidar measurement noise covariance matrix
+  MatrixXd R_lidar_;
 
   /**
    * Constructor
@@ -105,10 +94,15 @@ public:
   virtual ~UKF();
 
   /**
+   *  Angle normalization to [-Pi, Pi]
+   */
+  void NormAng(double *ang);
+
+  /**
    * ProcessMeasurement
    * @param meas_package The latest measurement data of either radar or laser
    */
-  void ProcessMeasurement(MeasurementPackage meas_package);
+  void ProcessMeasurement(MeasurementPackage measurement_pack);
 
   /**
    * Prediction Predicts sigma points, the state, and the state covariance
@@ -128,6 +122,11 @@ public:
    * @param meas_package The measurement at k+1
    */
   void UpdateRadar(MeasurementPackage meas_package);
+  /**
+   * Updates the state and the state covariance matrix of the UKF
+   *
+   */
+  void UpdateUKF(MeasurementPackage meas_package, MatrixXd Zsig, int n_z);
 };
 
 #endif /* UKF_H */
