1d7c74f94828252f849f98d408709285b7fcb223
Jeffrey Lutz <jeff.lutz@agileanswers.com>
2017-05-25 10:56:05 -0400

    Improved tools.cpp RMSE calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -37,7 +37,7 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   	}
 
   	//calculate the mean
-  	rmse = rmse/estimations.size();
+  	rmse /= estimations.size();
 
   	//calculate the squared root
   	rmse = rmse.array().sqrt();
